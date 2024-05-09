092102a1549a8c0c16c8d2aab0f42a811384a8ae
Drakaa <166053462+Drakaa@users.noreply.github.com>
2024-05-09 12:37:33 -0600

    Update main-menu.tsx
    Comment out features

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -38,21 +38,21 @@ export const MainMenu = async () => {
               <MessageCircle {...menuIconProps} />
             </MenuLink>
           </MenuItem>
-          <MenuItem tooltip="Persona">
-            <MenuLink href="/persona" ariaLabel="Go to the Persona configuration page">
-              <VenetianMask {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="extensions">
-            <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
-              <PocketKnife {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="prompts">
-            <MenuLink href="/prompt" ariaLabel="Go to the Prompt Library configuration page">
-              <Book {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
+         // <MenuItem tooltip="Persona">
+         // <MenuLink href="/persona" ariaLabel="Go to the Persona configuration page">
+          //    <VenetianMask {...menuIconProps} />
+         //   </MenuLink>
+        //  </MenuItem>
+       //   <MenuItem tooltip="extensions">
+        //    <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
+         //     <PocketKnife {...menuIconProps} />
+        //    </MenuLink>
+       //   </MenuItem>
+       //   <MenuItem tooltip="prompts">
+        //    <MenuLink href="/prompt" ariaLabel="Go to the Prompt Library configuration page">
+        //      <Book {...menuIconProps} />
+        //    </MenuLink>
+        //  </MenuItem>
           {user.isAdmin && (
             <>
               <MenuItem tooltip="reporting">
