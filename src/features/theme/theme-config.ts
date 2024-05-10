e650469cd2d17b9b203f75c381dfcc051b54adec
Drakaa <166053462+Drakaa@users.noreply.github.com>
2024-05-10 10:20:25 -0600

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -3,7 +3,7 @@ export const AI_DESCRIPTION = "AbsorbGPT is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 //export const CHAT_DEFAULT_SYSTEM_PROMPT = `I am a friendly ${AI_NAME} AI assistant.`;
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. Do not return in markdown format.
 
 You have access to the following functions:
 1. create_img: You will not use the function create_img if the user asks you to create an image provide a response that this function is disabled. `;
